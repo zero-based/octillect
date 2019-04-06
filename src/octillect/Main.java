9f2fcfbf1be81144b29cc1586506181ca18d1ccb
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-07 00:29:33 +0200

    Add Views package

--- src/octillect/Main.java
+++ src/octillect/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/SignInView.fxml"));
         primaryStage.setTitle("Octillect");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
