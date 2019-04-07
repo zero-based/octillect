9be48546b9b8e449ae0c548781a585dffcb8feb3
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-07 06:38:08 +0200

    Add Sign up View

--- src/octillect/controllers/SignUpController.java
+++ src/octillect/controllers/SignUpController.java
@@ -1,4 +1,27 @@
 package octillect.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import octillect.controls.OButton;
+
 public class SignUpController {
+
+    @FXML public HBox signUpHBox;
+    @FXML public OButton signUpButton;
+    @FXML public OButton signUpWithGitHubButton;
+    @FXML public OButton backButton;
+
+    @FXML
+    public void handleSignUpButtonAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleSignUpWithGitHubButtonAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleBackButtonAction(ActionEvent actionEvent) {
+    }
+
 }
