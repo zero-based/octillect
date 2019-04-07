fdbf54aa77a18ac43e65ff11b610215b6047ecc6
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-07 06:37:16 +0200

    Add Sign in View

--- src/octillect/controllers/SignInController.java
+++ src/octillect/controllers/SignInController.java
@@ -1,4 +1,29 @@
 package octillect.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import octillect.Main;
+import octillect.controls.OButton;
+
 public class SignInController {
+
+    @FXML public HBox signInHBox;
+    @FXML public OButton signInButton;
+    @FXML public OButton createAnAccountButton;
+
+    @FXML
+    public void handleSignInButtonAction(ActionEvent actionEvent) {
+        /* SIGN IN CODE HERE */
+        try {
+            Main.runApplication();
+        } catch( Exception e ) {
+            System.out.println("Couldn't start Application.");
+        }
+    }
+
+    @FXML
+    public void handleCreateAnAccountAction(ActionEvent actionEvent) {
+    }
+
 }
