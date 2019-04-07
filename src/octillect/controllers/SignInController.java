fd93d9ce3dc616586297b0655be619f1a73b8fcb
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-07 06:44:17 +0200

    Add basic Animation to Signing Stage
     * Create Ease In and Out animations to navigate between Sign in form
       and Sign up form.

--- src/octillect/controllers/SignInController.java
+++ src/octillect/controllers/SignInController.java
@@ -1,8 +1,16 @@
 package octillect.controllers;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 import octillect.Main;
 import octillect.controls.OButton;
 
@@ -23,7 +31,21 @@ public class SignInController {
     }
 
     @FXML
-    public void handleCreateAnAccountAction(ActionEvent actionEvent) {
+    public void handleCreateAnAccountAction(ActionEvent actionEvent) throws Exception{
+        // Initialize values
+        Parent signUpRoot = FXMLLoader.load(getClass().getResource("/octillect/views/SignUpView.fxml"));
+        StackPane parentStackPane = (StackPane) signInHBox.getParent();
+        double startValue = parentStackPane.getWidth();
+        double endValue = 0;
+        signUpRoot.translateXProperty().set(startValue);
+        parentStackPane.getChildren().add(signUpRoot);
+
+        // Animation
+        Timeline timeline = new Timeline();
+        KeyValue keyValue = new KeyValue(signUpRoot.translateXProperty(), endValue, Interpolator.EASE_IN);
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(650), keyValue);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
     }
 
 }
