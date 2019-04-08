1c5575042e639a6cc0d479affcee5260647b23f1
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-08 06:36:16 +0200

    Improve Palette Class
     * Get Colors from Palette.css using code to make sure that the Color
       hex code is only written once.

--- src/octillect/styles/Palette.java
+++ src/octillect/styles/Palette.java
@@ -1,16 +1,20 @@
 package octillect.styles;
 
 import javafx.scene.paint.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
 
 public final class Palette {
 
     /** Constant field; use Palette.PRIMARY */
-    public static final Color PRIMARY      = Color.valueOf("#1473e6");
-    public static final Color PRIMARY_DARK = Color.valueOf("#0f64d2");
-    public static final Color DARK_900     = Color.valueOf("#1b1b1b");
-    public static final Color DARK_700     = Color.valueOf("#323232");
-    public static final Color DARK_500     = Color.valueOf("#979797");
-    public static final Color DARK_300     = Color.valueOf("#cdcdcd");
+    public static final Color PRIMARY      = getColorFromCSS("-o-primary");
+    public static final Color PRIMARY_DARK = getColorFromCSS("-o-primary-dark");
+    public static final Color DARK_900     = getColorFromCSS("-o-dark-900");
+    public static final Color DARK_700     = getColorFromCSS("-o-dark-700");
+    public static final Color DARK_500     = getColorFromCSS("-o-dark-500");
+    public static final Color DARK_300     = getColorFromCSS("-o-dark-300");
 
     /** Getter method for FXML; use ${Palette.Primary} */
     public Color getPrimary() { return PRIMARY; }
@@ -19,5 +23,29 @@ public final class Palette {
     public Color getDark700() { return DARK_700; }
     public Color getDark500() { return DARK_500; }
     public Color getDark300() { return DARK_300; }
+
+    private static Color getColorFromCSS(String colorName) {
+        String colorHex = "";
+        try {
+            URL fileURL = Palette.class.getResource("Palette.css");
+            Scanner scanner = new Scanner(new File(fileURL.getPath()));
+            while (scanner.hasNextLine()) {
+                String buffer = scanner.nextLine();
+                if (buffer.contains(" " + colorName + " ")){
+                    colorHex = buffer.substring(buffer.indexOf('#'), buffer.indexOf('#') + 7);
+                    break;
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+
+        if (colorHex.equals("")){
+            System.out.println("Couldn't find the color name specified!");
+        }
+
+        return Color.valueOf(colorHex);
+    }
 
 }
