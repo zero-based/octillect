9caadd6fb2ba5b1cab6142a6fbbdd789383b05d9
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-04-09 16:16:34 +0200

    Improve firebase Connection
     * Initialize PROJECT_ID Variable.

--- src/octillect/database/firebase/Connection.java
+++ src/octillect/database/firebase/Connection.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Connection {
 
-    public static String PROJECT_ID;
+    public static final String PROJECT_ID           = "octillect-a01c0";
     public static final String DATABASE_URL         = "https://octillect-a01c0.firebaseio.com";
     public static final String SERVICE_ACCOUNT_KEY  = "src/octillect/database/firebase/serviceAccountKey.json";
     public static final String STORAGE_BUCKET_NAME  = "octillect-a01c0.appspot.com";
@@ -25,9 +25,7 @@ public class Connection {
                     .setDatabaseUrl(DATABASE_URL)
                     .setStorageBucket(STORAGE_BUCKET_NAME)
                     .build();
-
             FirebaseApp.initializeApp(options);
-            PROJECT_ID = FirebaseApp.getInstance().getOptions().getProjectId();
         } catch (IOException e) {
             e.printStackTrace();
         }
