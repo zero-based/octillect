ee23d34fe77bd03a60515571318923a4aa288552
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-04-11 21:20:08 +0200

    Modify UserRepository
     * Remove "add" method's body.
     * Make "encrypt" method static.
     * Format Code.

--- src/octillect/database/accessors/UserRepository.java
+++ src/octillect/database/accessors/UserRepository.java
@@ -1,28 +1,17 @@
 package octillect.database.accessors;
 
 import javafx.scene.image.Image;
-import octillect.database.firebase.FirestoreAPI;
 import octillect.database.firebase.StorageAPI;
 import octillect.models.User;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class UserRepository {
 
     // add new user data to database.
     public static void add(User user) {
-        // hash user's email here and set it as a user id
-        Map<String, Object> userMap = new HashMap<>();
-        userMap.put("name", user.getName());
-        userMap.put("email", user.getEmail());
-        userMap.put("password", user.getPassword());
-        FirestoreAPI.AddDocument(FirestoreAPI.USERS, user.getId(), userMap);
     }
 
     // add user's image to CloudStorage
@@ -36,7 +25,7 @@ public class UserRepository {
     }
 
     // Encryption method
-    public String encrypt(String textToEncrypt) {
+    public static String encrypt(String textToEncrypt) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
             byte[] messageDigest = md.digest(textToEncrypt.getBytes());
