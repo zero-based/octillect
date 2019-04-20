56b22a9cae86aa703742cabe69ebfc85831c4790
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-04-20 21:18:52 +0200

    Modify User Model
     * Update Project to be Projects array not just one Project.
     * Use Egyptian brackets with Getters and Setters.
     * Reformat code.

--- src/octillect/models/User.java
+++ src/octillect/models/User.java
@@ -9,36 +9,61 @@ public class User implements IObserver {
     private String email;
     private String password;
     private Image image;
-    private Project project;
+    private Project[] projects;
 
     public User() {}
 
-    public User(Project project) {
-        this.project = project;
+    public User(Project[] project) {
+        this.projects = project;
     }
 
     public User(String id, String name, String email, String password, Image image) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
+        this.id       = id;
+        this.name     = name;
+        this.email    = email;
         this.password = password;
-        this.image = image;
+        this.image    = image;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public String getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public Image getImage() { return image; }
-    public void setImage(Image image) { this.image = image; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
 
     @Override
     public void updateObserver() {
