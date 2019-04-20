5f1e608b82c05701dbea026789b295c711ef24c8
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-04-20 21:29:47 +0200

    Modify Task Model
     * Add id, creationDate, project, column, creator variables.
     * Rename Array of Users "assignee" to be "assignees".
     * Update the Copy Constructor with the new added variables.
     * Use Egyptian brackets with Getters and Setters.
     * Reformat code.

--- src/octillect/models/Task.java
+++ src/octillect/models/Task.java
@@ -4,41 +4,120 @@ import java.util.Date;
 
 public class Task {
 
+    private String id;
     private String name;
     private String description;
-    private User [] assignee;
+    private boolean isCompleted;
     private Date dueDate;
-    private Task [] subTasks;
-    private boolean isCompleted = false;
+    private Date creationDate;
+    private Project project;
+    private Column column;
+    private User creator;
+    private User[] assignees;
+    private Task[] subTasks;
 
     public Task() {}
 
-    public Task(String name, String description, User[] assignee, Date dueDate, Task[] subTasks, boolean isCompleted) {
-        this.name = name;
-        this.description = description;
-        this.assignee = assignee;
-        this.dueDate = dueDate;
-        this.subTasks = subTasks;
-        this.isCompleted = isCompleted;
+    public Task(String id, String name, String description, boolean isCompleted, Date dueDate, Date creationDate,
+                Project project, Column column, User creator, User[] assignees, Task[] subTasks) {
+        this.id           = id;
+        this.name         = name;
+        this.description  = description;
+        this.isCompleted  = isCompleted;
+        this.dueDate      = dueDate;
+        this.creationDate = creationDate;
+        this.project      = project;
+        this.column       = column;
+        this.creator      = creator;
+        this.assignees    = assignees;
+        this.subTasks     = subTasks;
     }
 
+    public String getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getName() {
+        return name;
+    }
 
-    public User[] getAssignee() { return assignee; }
-    public void setAssignee(User[] assignee) { this.assignee = assignee; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public Date getDueDate() { return dueDate; }
-    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
+    public String getDescription() {
+        return description;
+    }
 
-    public Task[] getSubTasks() { return subTasks; }
-    public void setSubTasks(Task[] subTasks) { this.subTasks = subTasks; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public boolean isCompleted() { return isCompleted;  }
-    public void setCompleted(boolean completed) { isCompleted = completed; }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
 
- }
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    public void setColumn(Column column) {
+        this.column = column;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public User[] getAssignees() {
+        return assignees;
+    }
+
+    public void setAssignees(User[] assignees) {
+        this.assignees = assignees;
+    }
+
+    public Task[] getSubTasks() {
+        return subTasks;
+    }
+
+    public void setSubTasks(Task[] subTasks) {
+        this.subTasks = subTasks;
+    }
+}
