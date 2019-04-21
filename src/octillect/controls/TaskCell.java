bcdfae84c734885b8d56f0c0b82ec4a61f8f3e2f
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-21 21:00:17 +0200

    Add Drag Event Handlers to TaskCell

--- src/octillect/controls/TaskCell.java
+++ src/octillect/controls/TaskCell.java
@@ -1,9 +1,18 @@
 package octillect.controls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 
 import octillect.models.Task;
@@ -12,6 +21,67 @@ public class TaskCell extends ListCell<Task> {
 
     @FXML private VBox taskCellVBox;
     @FXML private Label nameLabel;
+
+    public TaskCell() {
+
+        setOnDragDetected(event -> {
+
+            Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
+            Image snapshot = snapshot(new SnapshotParameters(), null);
+            dragboard.setDragView(snapshot, event.getX(), event.getY());
+
+            ClipboardContent clipboardContent = new ClipboardContent();
+            clipboardContent.putString(getItem().getId()); // Save the Source Task ID
+            dragboard.setContent(clipboardContent);
+
+        });
+
+        setOnDragOver(event -> {
+            if (event.getGestureSource() instanceof TaskCell && getItem() != null) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
+        });
+
+        setOnDragEntered(event -> {
+            if (event.getGestureSource() instanceof TaskCell && getItem() != null) {
+
+                Dragboard dragboard = event.getDragboard();
+                ListView currentListView = getListView();
+                Task task = null;
+
+                // Get all Tasks ListViews in the current Project
+                List<ListView<Task>> allTasksListViews = new ArrayList<>();
+
+                this.getListView()                 // Gets tasksListView
+                        .getParent()               // Gets tasksColumnVBox
+                        .getParent()               // Gets ListCell<Column>
+                        .getParent()               // Gets projectListView
+                        .getChildrenUnmodifiable() // Gets All ListCell<Column>'s
+                        .forEach(tasksColumn -> {
+                            if (((TasksColumn) tasksColumn).getTasksListView() != null)
+                                allTasksListViews.add(((TasksColumn) tasksColumn).getTasksListView());
+                        });
+
+                // Get the Source Task using ID
+                for (ListView<Task> listView : allTasksListViews) {
+                    for (Task currentTask : listView.getItems()) {
+                        if (currentTask.getId() == dragboard.getString()) {
+                            task = currentTask;
+                        }
+                    }
+                }
+
+                // Remove the Source Task from all Columns
+                for (ListView<Task> listView : allTasksListViews) {
+                    listView.getItems().remove(task);
+                }
+
+                // Add task to current position
+                currentListView.getItems().add(getIndex(), task);
+            }
+        });
+
+    }
 
     @Override
     public void updateItem(Task taskItem, boolean empty) {
