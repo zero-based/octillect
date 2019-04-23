06946a67f3c259d664ebcbedfd60d4edead9b9f3
Monica Atef <monicaatef46@gmail.com>
2019-04-23 20:40:58 +0200

    Add Drawers Views & Controllers

---
+++ src/octillect/controllers/RightDrawerController.java
@@ -0,0 +1,15 @@
+package octillect.controllers;
+
+import com.jfoenix.controls.JFXDrawer;
+
+import javafx.fxml.FXML;
+
+public class RightDrawerController {
+
+    @FXML private JFXDrawer rightDrawer;
+
+    public JFXDrawer getRightDrawer() {
+        return rightDrawer;
+    }
+
+}
