52cd84d194b18a1b6ae42fe83001956694d92dd2
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-26 23:15:57 +0200

    Improve Palette
     * Load the CSS file into hash map then populate the colors using
       the hash map.
     * Use Static block initialization to initialize the colors and access
       the file only once.

--- src/octillect/styles/Palette.java
+++ src/octillect/styles/Palette.java
@@ -2,6 +2,8 @@ package octillect.styles;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 import javafx.scene.paint.Color;
@@ -14,19 +16,51 @@ public final class Palette {
     /**
      * Constant field; use Palette.PRIMARY
      */
-    public static final Color PRIMARY      = getColorFromCSS("-o-primary");
-    public static final Color PRIMARY_DARK = getColorFromCSS("-o-primary-dark");
-    public static final Color DARK_900     = getColorFromCSS("-o-dark-900");
-    public static final Color DARK_700     = getColorFromCSS("-o-dark-700");
-    public static final Color DARK_500     = getColorFromCSS("-o-dark-500");
-    public static final Color DARK_300     = getColorFromCSS("-o-dark-300");
+    public static final Color PRIMARY;
+    public static final Color PRIMARY_DARK;
+    public static final Color DARK_900;
+    public static final Color DARK_700;
+    public static final Color DARK_500;
+    public static final Color DARK_300;
 
+    static {
+
+        // Populate the palette hash map from the Palette CSS file.
+
+        HashMap<String, Color> palette = new HashMap<>();
+        try {
+            Scanner scanner = new Scanner(new File(CSS_LOCATION + CSS_FILE_NAME));
+            while (scanner.hasNextLine()) {
+                String buffer = scanner.nextLine();
+                if (buffer.contains(";")) {
+                    buffer         = buffer.replace(" ", "");
+                    String name    = buffer.substring(0, buffer.indexOf(':'));
+                    String hexCode = buffer.substring(buffer.indexOf('#'), buffer.indexOf('#') + 7);
+                    palette.put(name, Color.valueOf(hexCode));
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        // Initialize the Colors
+
+        PRIMARY      = palette.get("-o-primary");
+        PRIMARY_DARK = palette.get("-o-primary-dark");
+        DARK_900     = palette.get("-o-dark-900");
+        DARK_700     = palette.get("-o-dark-700");
+        DARK_500     = palette.get("-o-dark-500");
+        DARK_300     = palette.get("-o-dark-300");
+
+    }
 
     /**
      * Getter method for FXML;
      * put the following at the beginning of the FXML file:
      * <fx:define>
-     *     <octillect.styles.Palette fx:id="Palette" />
+     *     <fx:id="Palette" />
      * </fx:define>
      * then use ${Palette.PRIMARY} for binding.
      */
@@ -36,33 +70,5 @@ public final class Palette {
     public Color getDARK_700() { return DARK_700; }
     public Color getDARK_500() { return DARK_500; }
     public Color getDARK_300() { return DARK_300; }
-
-
-    /**
-     * Looks up for the color in the Palette CSS file then gets its
-     * HEX code and return a Color object having the same HEX code.
-     */
-    private static Color getColorFromCSS(String colorName) {
-        String colorHexCode = "";
-        try {
-            Scanner scanner = new Scanner(new File(CSS_LOCATION + CSS_FILE_NAME));
-            while (scanner.hasNextLine()) {
-                String buffer = scanner.nextLine();
-                if (buffer.contains(" " + colorName + " ")){
-                    colorHexCode = buffer.substring(buffer.indexOf('#'), buffer.indexOf('#') + 7);
-                    break;
-                }
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if (colorHexCode.equals("")){
-            System.out.println(colorName + " is not found in " + CSS_FILE_NAME);
-        }
-
-        return Color.valueOf(colorHexCode);
-    }
 
 }
