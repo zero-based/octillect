9b8fd8acdda50bd92013574a8d235179b841ad6d
Monica Tanios <monicatanios@gmail.com>
2019-04-26 23:10:32 +0200

    Set Project Name in Title Bar

--- src/octillect/controllers/ProjectController.java
+++ src/octillect/controllers/ProjectController.java
@@ -1,11 +1,13 @@
 package octillect.controllers;
 
 import com.jfoenix.controls.JFXTextField;
+
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-
 import javafx.scene.input.MouseEvent;
+
 import octillect.controls.TasksColumn;
 import octillect.models.Project;
 
@@ -20,15 +22,23 @@ public class ProjectController implements Injectable<ApplicationController> {
 
     // Injected Controllers
     private ApplicationController applicationController;
+    private TitleBarController titleBarController;
 
     @Override
     public void inject(ApplicationController applicationController) {
         this.applicationController = applicationController;
+        titleBarController         = applicationController.titleBarController;
     }
 
     public void loadProject(Project project) {
+
+        // Set Project Name in the Title Bar
+        titleBarController.projectNameLabel.setText(project.getName());
+
+        // Populate Project Columns
         projectListView.setItems(project.getColumns());
         projectListView.setCellFactory(param -> new TasksColumn());
+
     }
 
     public void handleGitHubIconAction(MouseEvent mouseEvent) {
