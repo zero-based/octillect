ab45fca1536e24145bc5b18004aa3d5a7f038948
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-04-28 06:19:26 +0200

    Improve SignUpController
     * Improve TextFields' validation.
     * Avoid multiple setting of SignUpButton's action handler.
     * Generate an Identicon only once.
     * Remove getChosenImage method to avoid process repetition and replace
       it with a BufferedImage variable that is initialized when a photo is
       chosen.
     * Remove the else block in ImageButton action handler as the user can't
       choose "nothing" from the file chooser, once an image is chosen it's
       permanent.

--- src/octillect/controllers/SignUpController.java
+++ src/octillect/controllers/SignUpController.java
@@ -6,18 +6,19 @@ import com.jfoenix.validation.RegexValidator;
 import com.jfoenix.validation.RequiredFieldValidator;
 
 import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 import javax.imageio.ImageIO;
 
@@ -42,148 +43,124 @@ public class SignUpController {
     @FXML private JFXPasswordField passwordTextField;
     @FXML private JFXPasswordField confirmPasswordTextField;
 
-    private RequiredFieldValidator requiredFieldValidator = new RequiredFieldValidator();
-    private RegexValidator emailValidator                 = new RegexValidator();
-    private RegexValidator emailUsedValidator             = new RegexValidator();
-    private RegexValidator passwordValidator              = new RegexValidator();
-    private RegexValidator confirmPasswordValidator       = new RegexValidator();
+    private RequiredFieldValidator requiredFieldValidator;
+    private RegexValidator emailValidator;
+    private RegexValidator emailUsedValidator;
+    private RegexValidator passwordValidator;
+    private RegexValidator confirmPasswordValidator;
 
-    private String chosenImagePath = null;
+    private BufferedImage chosenImage = null;
 
-    // TextFields Validation
     @FXML
     public void initialize() {
-        firstNameTextField.getValidators().add(requiredFieldValidator);
-        lastNameTextField .getValidators().add(requiredFieldValidator);
-        emailTextField    .getValidators().add(emailValidator);
 
-        requiredFieldValidator.setMessage("Required field.");
+        requiredFieldValidator   = new RequiredFieldValidator("Required field.");
+        emailValidator           = new RegexValidator("Invalid Email.");
+        emailUsedValidator       = new RegexValidator("This email already have an account. Try another.");
+        passwordValidator        = new RegexValidator("Use 8 or more characters with a mix of letters and numbers.");
+        confirmPasswordValidator = new RegexValidator("Those passwords didn't match. Try again.");
 
-        emailValidator.setRegexPattern("([a-z0-9_\\.-]+)@[\\da-z\\.-]+[a-z\\.]{2,5}");
-        emailValidator.setMessage("Invalid Email.");
+        emailValidator          .setRegexPattern("([a-z0-9_\\.-]+)@[\\da-z\\.-]+[a-z\\.]{2,5}");
+        emailUsedValidator      .setRegexPattern("^((?!.*" + emailTextField.getText() + ".*).)*$");
+        passwordValidator       .setRegexPattern("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$");
 
-        emailUsedValidator.setRegexPattern("^((?!.*"+ emailTextField.getText() +".*).)*$");
-
-        passwordValidator.setRegexPattern("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$");
-        passwordValidator.setMessage("Use 8 or more characters with a mix of letters and numbers.");
-
-        confirmPasswordValidator.setMessage("Those passwords didn't match. Try again.");
+        firstNameTextField      .getValidators().add(requiredFieldValidator);
+        lastNameTextField       .getValidators().add(requiredFieldValidator);
+        emailTextField          .getValidators().add(emailValidator);
+        passwordTextField       .getValidators().add(passwordValidator);
+        confirmPasswordTextField.getValidators().add(requiredFieldValidator);
 
         firstNameTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
                 firstNameTextField.validate();
-                signUpButton.setOnAction(this::handleSignUpButtonAction);
             }
         });
 
         lastNameTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
                 lastNameTextField.validate();
-                signUpButton.setOnAction(this::handleSignUpButtonAction);
             }
         });
 
         emailTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            emailTextField.getValidators().remove(emailUsedValidator);
             if (!newValue) {
                 emailTextField.validate();
-                signUpButton.setOnAction(this::handleSignUpButtonAction);
             }
         });
 
         passwordTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            passwordTextField.getValidators().add(passwordValidator);
             if (!newValue) {
                 passwordTextField.validate();
-                signUpButton.setOnAction(this::handleSignUpButtonAction);
             }
         });
 
         confirmPasswordTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            confirmPasswordTextField.getValidators().add(confirmPasswordValidator);
             confirmPasswordValidator.setRegexPattern(passwordTextField.getText());
-            if (!newValue && !passwordTextField.getText().equals("")) {
-                confirmPasswordTextField.getValidators().add(confirmPasswordValidator);
+            if (!newValue) {
                 confirmPasswordTextField.validate();
-                signUpButton.setOnAction(this::handleSignUpButtonAction);
+                confirmPasswordTextField.getValidators().remove(confirmPasswordValidator);
             }
         });
     }
 
     @FXML
     public void handleSignUpButtonAction(ActionEvent actionEvent) {
+
         firstNameTextField      .validate();
         lastNameTextField       .validate();
         emailTextField          .validate();
-        passwordTextField       .getValidators().add(requiredFieldValidator);
         passwordTextField       .validate();
-        confirmPasswordTextField.getValidators().add(requiredFieldValidator);
         confirmPasswordTextField.validate();
 
-        // set Sign up Button's action handler to null when there's any Validation errors
-        if (requiredFieldValidator.getHasErrors() || emailValidator.getHasErrors() || passwordValidator.getHasErrors()) {
-            signUpButton.setOnAction(null);
-        } else if (UserRepository.get(FirestoreAPI.encrypt(emailTextField.getText())) != null) {
-            emailUsedValidator.setMessage("This email already have an account. Try another.");
-            emailTextField.getValidators().add(emailUsedValidator);
-            emailTextField.validate();
-            signUpButton.setOnAction(null);
-        } else {
-            User user = new UserBuilder().with($ -> {
-                $.id       = FirestoreAPI.encrypt(emailTextField.getText());
-                $.email    = emailTextField.getText();
-                $.password = FirestoreAPI.encrypt(passwordTextField.getText());
-                $.name     = firstNameTextField.getText() + " " + lastNameTextField.getText();
-            }).build();
+        if (!requiredFieldValidator.getHasErrors() && !emailValidator.getHasErrors()
+            && !passwordValidator.getHasErrors() && !confirmPasswordValidator.getHasErrors()) {
 
-            UserRepository.add(user);
-
-            if (chosenImagePath != null) {
-                UserRepository.setImage(user.getId(), getChosenImage(chosenImagePath));
-                user.setImage(SwingFXUtils.toFXImage(getChosenImage(chosenImagePath), null));
+            if (UserRepository.get(FirestoreAPI.encrypt(emailTextField.getText())) != null) {
+                emailTextField.getValidators().add(emailUsedValidator);
+                emailTextField.validate();
+                emailTextField.getValidators().remove(emailUsedValidator);
             } else {
-                // Generate an Identicon for the user in case of not choosing a photo
-                UserRepository.setImage(user.getId(), UserRepository.generateIdenticon(user.getId(), 256));
-                Image image = SwingFXUtils.toFXImage(UserRepository.generateIdenticon(user.getId(), 256), null);
-                user.setImage(image);
-            }
+                User user = new UserBuilder().with($ -> {
+                    $.id = FirestoreAPI.encrypt(emailTextField.getText());
+                    $.email = emailTextField.getText();
+                    $.password = FirestoreAPI.encrypt(passwordTextField.getText());
+                    $.name = firstNameTextField.getText() + " " + lastNameTextField.getText();
+                }).build();
 
-            Main.runApplication(user);
+                UserRepository.add(user);
+
+                if (chosenImage != null) {
+                    UserRepository.setImage(user.getId(), chosenImage);
+                    user.setImage(SwingFXUtils.toFXImage(chosenImage, null));
+                } else {
+                    // Generate an Identicon for the user in case of not choosing a photo.
+                    BufferedImage identicon = UserRepository.generateIdenticon(user.getId(), 256);
+                    UserRepository.setImage(user.getId(), identicon);
+                    user.setImage(SwingFXUtils.toFXImage(identicon, null));
+                }
+
+                Main.runApplication(user);
+
+            }
         }
     }
 
     @FXML
-    public void handleImageButtonAction(ActionEvent actionEvent) {
+    public void handleImageButtonAction(ActionEvent actionEvent) throws IOException {
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose a Profile picture");
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image files", "*.jpg", "*.png"));
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Image files", "*.jpg", "*.png"));
         File file = fileChooser.showOpenDialog(Main.signingStage);
 
         if (file != null) {
-            chosenImagePath = file.getPath();
-            ImagePattern imagePattern = new ImagePattern(SwingFXUtils.toFXImage(getChosenImage(chosenImagePath), null));
-            userImage.setFill(imagePattern);
+            chosenImage = ImageIO.read(new File(file.getPath()));
+            userImage.setFill(new ImagePattern(SwingFXUtils.toFXImage(chosenImage, null)));
             userImage.setOpacity(100);
             imageButton.setOpacity(0);
-        } else {
-            // This Code Block Handles choosing a picture then opening file chooser and then choose nothing
-            imageButton.setOpacity(100);
-            userImage.setFill(null);
         }
-    }
 
-    /**
-     * Converts image to a BufferedImage.
-     *
-     * @param imagePath Image File path.
-     * @return BufferedImage of the given image.
-     */
-    public BufferedImage getChosenImage(String imagePath) {
-        try {
-            return ImageIO.read(new File(imagePath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     @FXML
