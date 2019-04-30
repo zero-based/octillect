245805b72180bd7c80f6ea7b75291e3c09d950b5
Micheline Medhat <MichelineMedhat@gmail.com>
2019-05-01 00:35:57 +0200

    Add Task Document/Repository

---
+++ src/octillect/database/accessors/TaskRepository.java
@@ -0,0 +1,41 @@
+package octillect.database.accessors;
+
+import java.util.ArrayList;
+
+import octillect.database.documents.TaskDocument;
+import octillect.database.firebase.FirestoreAPI;
+import octillect.models.Task;
+import octillect.models.User;
+
+public class TaskRepository {
+
+    // Add new task data to database.
+    public static void add(Task task) {
+        TaskDocument document = new TaskDocument();
+        document.setId(task.getId());
+        document.setName(task.getName());
+        document.setDescription(task.getDescription());
+        document.setIsCompleted(task.isCompleted());
+        document.setDueDate(task.getDueDate());
+        document.setCreationDate(task.getCreationDate());
+        document.setProjectId(task.getProject().getId());
+        document.setColumnId(task.getColumn().getId());
+        document.setCreatorId(task.getCreator().getId());
+
+        if (task.getAssignees() != null) {
+            ArrayList<String> assigneesIds = new ArrayList<>();
+            for (User assignee : task.getAssignees()) {
+                assigneesIds.add(assignee.getId());
+            }
+            document.setAssigneesIds(assigneesIds);
+        }
+        if (task.getSubTasks() != null) {
+            ArrayList<String> subTasksIds = new ArrayList<>();
+            for (Task subTasks : task.getSubTasks()) {
+                subTasksIds.add(subTasks.getId());
+            }
+            document.setSubTasksIds(subTasksIds);
+        }
+        FirestoreAPI.insertDocument(FirestoreAPI.TASKS, document.getId(), document);
+    }
+}
