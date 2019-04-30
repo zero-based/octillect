a9f22961e6d1c64febdd34b8e18611bae78bddbb
Micheline Medhat <MichelineMedhat@gmail.com>
2019-05-01 00:40:32 +0200

    Add Column Document/Repository

---
+++ src/octillect/database/accessors/ColumnRepository.java
@@ -0,0 +1,28 @@
+package octillect.database.accessors;
+
+import java.util.ArrayList;
+
+import octillect.database.documents.ColumnDocument;
+import octillect.database.firebase.FirestoreAPI;
+import octillect.models.Column;
+import octillect.models.Task;
+
+public class ColumnRepository {
+
+    // Add new column data to database.
+    public static void add(Column column) {
+        ColumnDocument document = new ColumnDocument();
+        document.setId(column.getId());
+        document.setName(column.getName());
+        document.setProjectId(column.getProject().getId());
+
+        if (column.getTasks() != null) {
+            ArrayList<String> tasksIds= new ArrayList<>();
+            for (Task task : column.getTasks()) {
+                tasksIds.add(task.getId());
+            }
+            document.setTasksIds(tasksIds);
+        }
+        FirestoreAPI.insertDocument(FirestoreAPI.COLUMNS, document.getId(), document);
+    }
+}
