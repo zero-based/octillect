fa561d474dd5ac19fe2ecb7568b4f0947afaef0d
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-06 00:40:05 +0200

    Modify TaskSettings View
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- src/octillect/controllers/TaskSettingsController.java
+++ src/octillect/controllers/TaskSettingsController.java
@@ -1,20 +1,43 @@
 package octillect.controllers;
 
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 import org.controlsfx.control.CheckComboBox;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 public class TaskSettingsController implements Injectable<ApplicationController> {
 
     // FXML Fields
-    @FXML public CheckComboBox<String> addFeaturesCheckComboBox;
-    @FXML public CheckComboBox<String> addLabelsCheckComboBox;
+    @FXML public TitledPane editNameTitledPane;
+    @FXML public TitledPane editDescriptionTitledPane;
+    @FXML public TitledPane assigneesTitledPane;
+    @FXML public TitledPane labelsTitledPane;
+    @FXML public TitledPane dueDateTitledPane;
+    @FXML public TitledPane isCompletedTaskTitledPane;
+    @FXML public TitledPane deleteTaskTitledPane;
+    @FXML public Circle taskCreatorImageCircle;
+    @FXML public FontIcon isCompletedTaskIcon;
+    @FXML public Label taskCreatorLabel;
+    @FXML public Label taskCreationDateLabel;
+    @FXML public JFXTextField taskNameTextField;
+    @FXML public JFXTextArea taskDescriptionTextArea;
+    @FXML public JFXDatePicker taskDueDatePicker;
+    @FXML public CheckComboBox<String> assigneesCheckComboBox;
+    @FXML public CheckComboBox<String> labelsCheckComboBox;
 
     public ObservableList<String> selectedAssignees;
     public ObservableList<String> selectedLabels;
+
 
     // Injected Controllers
     private ApplicationController applicationController;
@@ -32,12 +55,12 @@ public class TaskSettingsController implements Injectable<ApplicationController>
         for (int i = 0; i <= 10; i++) {
             assignees.add("items" + i);
         }
-        addFeaturesCheckComboBox.getItems().addAll(assignees);
+        assigneesCheckComboBox.getItems().addAll(assignees);
 
-        addFeaturesCheckComboBox.getCheckModel()
+        assigneesCheckComboBox.getCheckModel()
                 .getCheckedItems()
                 .addListener((ListChangeListener<String>) c -> {
-                    selectedAssignees = addFeaturesCheckComboBox.getCheckModel()
+                    selectedAssignees = assigneesCheckComboBox.getCheckModel()
                             .getCheckedItems();
                 });
 
@@ -46,13 +69,32 @@ public class TaskSettingsController implements Injectable<ApplicationController>
         for (int i = 0; i <= 10; i++) {
             labels.add("items" + i);
         }
-        addLabelsCheckComboBox.getItems().addAll(labels);
+        labelsCheckComboBox.getItems().addAll(labels);
 
-        addLabelsCheckComboBox.getCheckModel()
+        labelsCheckComboBox.getCheckModel()
                 .getCheckedItems()
                 .addListener((ListChangeListener<String>) c -> {
-                    selectedLabels = addLabelsCheckComboBox.getCheckModel()
+                    selectedLabels = labelsCheckComboBox.getCheckModel()
                             .getCheckedItems();
                 });
     }
+
+    @FXML
+    public void handleTitledPaneOnAction(MouseEvent mouseEvent) {
+        editNameTitledPane.setExpanded(false);
+        editDescriptionTitledPane.setExpanded(false);
+        assigneesTitledPane.setExpanded(false);
+        labelsTitledPane.setExpanded(false);
+        dueDateTitledPane.setExpanded(false);
+        ((TitledPane) mouseEvent.getSource()).setExpanded(true);
+    }
+
+    @FXML
+    public void handleIsCompletedTaskAction(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void handleDeleteTaskAction(MouseEvent mouseEvent) {
+    }
+
 }
