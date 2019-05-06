269b722ab04f1fafb164a7afbf11989a833901d2
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-07 01:09:33 +0200

    Modify Contributor/Label Cell
     * Inject ApplicationController in both cells.
     * Set Circle Fill with Contributor Image.
     * Refactor Code.
    
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- src/octillect/controls/ContributorCell.java
+++ src/octillect/controls/ContributorCell.java
@@ -5,21 +5,39 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.util.Pair;
 
+import octillect.controllers.ApplicationController;
+import octillect.controllers.Injectable;
 import octillect.models.Project;
 import octillect.models.User;
 
 import org.kordamp.ikonli.javafx.FontIcon;
 
-public class ContributorCell extends ListCell<Pair<User, Project.Role>> {
+public class ContributorCell extends ListCell<Pair<User, Project.Role>> implements Injectable<ApplicationController> {
 
     //FXML Fields
-    @FXML public BorderPane contributorCellBorderPane;
-    @FXML public Label usernameLabel;
-    @FXML public Label emailLabel;
-    @FXML public Label roleLabel;
-    @FXML public FontIcon closeIcon;
+    @FXML private BorderPane contributorCellBorderPane;
+    @FXML private Circle contributorImage;
+    @FXML private Label usernameLabel;
+    @FXML private Label emailLabel;
+    @FXML private Label roleLabel;
+    @FXML private FontIcon closeIcon;
+
+    //Injected Controllers
+    private ApplicationController applicationController;
+
+    @Override
+    public void inject(ApplicationController applicationController) {
+        this.applicationController = applicationController;
+    }
+
+    @Override
+    public void init() {
+        throw new UnsupportedOperationException("ContributorCell cannot be initialized");
+    }
 
     @Override
     public void updateItem(Pair<User, Project.Role> contributorItem, boolean empty) {
@@ -39,6 +57,9 @@ public class ContributorCell extends ListCell<Pair<User, Project.Role>> {
             e.printStackTrace();
         }
 
+        if (contributorItem.getKey().getImage() != null) {
+            contributorImage.setFill(new ImagePattern(contributorItem.getKey().getImage()));
+        }
         usernameLabel.setText(contributorItem.getKey().getName());
         emailLabel.setText(contributorItem.getKey().getEmail());
         roleLabel.setText(contributorItem.getValue().toString());
--- src/octillect/controls/LabelCell.java
+++ src/octillect/controls/LabelCell.java
@@ -6,17 +6,34 @@ import javafx.scene.control.ListCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import javafx.scene.paint.Color;
+import octillect.controllers.ApplicationController;
+import octillect.controllers.Injectable;
 import octillect.models.Label;
 
 import org.kordamp.ikonli.javafx.FontIcon;
 
-public class LabelCell extends ListCell<Label> {
+public class LabelCell extends ListCell<Label> implements Injectable<ApplicationController> {
 
     //FXML Fields
-    @FXML public BorderPane labelCellBorderPane;
-    @FXML public HBox labelColorHBox;
-    @FXML public javafx.scene.control.Label labelNameLabel;
-    @FXML public FontIcon deleteLabelIcon;
+    @FXML private BorderPane labelCellBorderPane;
+    @FXML private HBox labelColorHBox;
+    @FXML private javafx.scene.control.Label labelNameLabel;
+    @FXML private FontIcon deleteLabelIcon;
+
+    // Injected Controllers
+    private ApplicationController applicationController;
+
+    @Override
+    public void inject(ApplicationController applicationController) {
+        this.applicationController = applicationController;
+    }
+
+    @Override
+    public void init() {
+        throw new UnsupportedOperationException("LabelCell cannot be initialized");
+    }
+
 
     public void updateItem(Label labelItem, boolean empty) {
 
@@ -37,20 +54,23 @@ public class LabelCell extends ListCell<Label> {
 
         labelColorHBox.setStyle("-fx-background-color: " + labelItem.getColorHex() + "; -fx-background-radius: 4px;");
         labelNameLabel.setText(labelItem.getName());
-
-        // Counting the perceptive luminance
-        double luminance = 0.299 * labelItem.getColor().getRed()
-                + 0.587 * labelItem.getColor().getGreen()
-                + 0.114 * labelItem.getColor().getBlue();
-
-        String textFillStyle = luminance > 0.5 ? "-fx-text-fill: black;" : "-fx-text-fill: white;";
-        labelNameLabel.setStyle(textFillStyle);
+        labelNameLabel.setStyle(determineTextFillStyle((labelItem.getColor())));
 
         deleteLabelIcon.setOnMouseClicked(event -> {
         });
 
         setGraphic(labelCellBorderPane);
 
+    }
+
+    private String determineTextFillStyle(Color labelColor){
+
+        // Counting the perceptive luminance
+        double luminance = 0.299 * labelColor.getRed()
+                + 0.587 * labelColor.getGreen()
+                + 0.114 * labelColor.getBlue();
+
+        return luminance > 0.5 ? "-fx-text-fill: black;" : "-fx-text-fill: white;";
     }
 
 }
