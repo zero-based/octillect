cfc1581f119e56491ee891f1308d4524c0bb8b27
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-07 01:44:09 +0200

    Add Validators to input Controls
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- src/octillect/controllers/ProjectSettingsController.java
+++ src/octillect/controllers/ProjectSettingsController.java
@@ -5,6 +5,8 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RegexValidator;
+import com.jfoenix.validation.RequiredFieldValidator;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -19,6 +21,8 @@ import javafx.util.StringConverter;
 import octillect.controls.ContributorCell;
 import octillect.controls.LabelCell;
 import octillect.controls.OButton;
+import octillect.database.accessors.UserRepository;
+import octillect.database.firebase.FirestoreAPI;
 import octillect.models.Project;
 import octillect.models.User;
 import octillect.models.builders.LabelBuilder;
@@ -45,16 +49,31 @@ public class ProjectSettingsController implements Injectable<ApplicationControll
     @FXML public OButton addLabelButton;
     @FXML public TitledPane deleteProjectTitledPane;
 
+    // Validators
+    private RequiredFieldValidator requiredFieldValidator;
+    private RegexValidator emailValidator;
+
     // Injected Controllers
     private ApplicationController applicationController;
+    ProjectController projectController;
 
     @Override
     public void inject(ApplicationController applicationController) {
         this.applicationController = applicationController;
+        projectController          = applicationController.projectController;
     }
 
     @Override
     public void init() {
+
+        requiredFieldValidator = new RequiredFieldValidator("Required field.");
+        emailValidator = new RegexValidator();
+        emailValidator.setRegexPattern("^((?!.*" + inviteContributorByEmailTextField.getText() + ".*).)*$");
+
+        newLabelTextField.getValidators().add(requiredFieldValidator);
+        rolesComboBox.getValidators().add(requiredFieldValidator);
+        inviteContributorByEmailTextField.getValidators().add(requiredFieldValidator);
+        inviteContributorByEmailTextField.getValidators().add(emailValidator);
 
         //Initializing rolesComboBox
         for (Project.Role role : Project.Role.values()) {
@@ -93,7 +112,11 @@ public class ProjectSettingsController implements Injectable<ApplicationControll
                 new Pair(user2, Project.Role.admin),
                 new Pair(user3, Project.Role.viewer));
         contributorsListView.setItems(contributors);
-        contributorsListView.setCellFactory(param -> new ContributorCell());
+        contributorsListView.setCellFactory(param -> {
+            ContributorCell contributorCell = new ContributorCell();
+            contributorCell.inject(applicationController);
+            return contributorCell;
+        });
 
         //Initializing labelsListView
         octillect.models.Label label1 = new LabelBuilder().with($_label1 -> {
@@ -111,7 +134,11 @@ public class ProjectSettingsController implements Injectable<ApplicationControll
 
         ObservableList<octillect.models.Label> labels = FXCollections.observableArrayList(label1, label2, label3);
         labelsListView.setItems(labels);
-        labelsListView.setCellFactory(param -> new LabelCell());
+        labelsListView.setCellFactory(param -> {
+            LabelCell labelCell = new LabelCell();
+            labelCell.inject(applicationController);
+            return labelCell;
+        });
 
     }
 
@@ -130,12 +157,66 @@ public class ProjectSettingsController implements Injectable<ApplicationControll
     }
 
     public void handleInviteContributorButtonAction(MouseEvent mouseEvent) {
+
+        resetRequiredFieldValidators();
+        inviteContributorByEmailTextField.validate();
+        rolesComboBox.validate();
+        boolean isContributor = false;
+
+        if (!requiredFieldValidator.getHasErrors()) {
+
+            for (Pair<User, Project.Role> contributor : projectController.currentProject.getContributors()) {
+                if (contributor.getKey().getEmail().equals(inviteContributorByEmailTextField.getText())) {
+                    isContributor = true;
+                }
+            }
+
+            if (isContributor) {
+                emailValidator.setMessage("Already a Contributor.");
+                inviteContributorByEmailTextField.getValidators().add(emailValidator);
+                inviteContributorByEmailTextField.validate();
+                inviteContributorByEmailTextField.getValidators().remove(emailValidator);
+            } else {
+                User user = UserRepository.get(FirestoreAPI.encrypt(inviteContributorByEmailTextField.getText()));
+
+                if (user == null) {
+                    emailValidator.setMessage("That Octillect account doesn't exist.");
+                    inviteContributorByEmailTextField.getValidators().add(emailValidator);
+                    inviteContributorByEmailTextField.validate();
+                    inviteContributorByEmailTextField.getValidators().remove(emailValidator);
+                } else {
+                    /*TODO: Add Contributor to Database/ListView Here.*/
+                    resetRequiredFieldValidators();
+                }
+            }
+        }
     }
 
     public void handleAddLabelButtonAction(MouseEvent mouseEvent) {
+
+        resetRequiredFieldValidators();
+        newLabelTextField.validate();
+
+        if (!requiredFieldValidator.getHasErrors()) {
+
+            octillect.models.Label label = new LabelBuilder().with($ -> {
+                $.id = FirestoreAPI.encryptWithDateTime(newLabelTextField.getText() + applicationController.user.getId());
+                $.name = newLabelTextField.getText();
+                $.color = labelColorPicker.getValue();
+            }).build();
+
+            /*TODO: Add Label to Database/ListView Here.*/
+            resetRequiredFieldValidators();
+        }
     }
 
     public void handleDeleteProjectAction(MouseEvent mouseEvent) {
+    }
+
+    private void resetRequiredFieldValidators() {
+        inviteContributorByEmailTextField.resetValidation();
+        newLabelTextField.resetValidation();
+        rolesComboBox.resetValidation();
     }
 
 }
