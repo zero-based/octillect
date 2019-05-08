9a890e143328b12a2dc06d2df712f167f4c47f59
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-08 08:32:45 +0200

    Add SubTask Back-end

--- src/octillect/controls/SubTaskCell.java
+++ src/octillect/controls/SubTaskCell.java
@@ -6,16 +6,36 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.BorderPane;
 
+import octillect.controllers.ApplicationController;
+import octillect.controllers.Injectable;
+import octillect.controllers.settings.TaskSettingsController;
+import octillect.database.repositories.TaskRepository;
 import octillect.models.Task;
 
 import org.kordamp.ikonli.javafx.FontIcon;
 
-public class SubTaskCell extends ListCell<Task> {
+public class SubTaskCell extends ListCell<Task> implements Injectable<ApplicationController> {
 
     // FXML Fields
     @FXML private BorderPane subTaskBorderPane;
     @FXML private CheckBox subTaskCheckBox;
     @FXML private FontIcon deleteSubTaskIcon;
+
+    // Injected Controllers
+    private ApplicationController applicationController;
+    private TaskSettingsController taskSettingsController;
+
+
+    @Override
+    public void inject(ApplicationController applicationController) {
+        this.applicationController = applicationController;
+        taskSettingsController     = applicationController.rightDrawerController.taskSettingsController;
+    }
+
+    @Override
+    public void init() {
+        throw new UnsupportedOperationException("SubTaskCell cannot be initialized");
+    }
 
     public void updateItem(Task subTaskItem, boolean empty) {
 
@@ -36,14 +56,18 @@ public class SubTaskCell extends ListCell<Task> {
 
         subTaskCheckBox.setText(subTaskItem.getName());
         subTaskCheckBox.setSelected(subTaskItem.getIsCompleted());
-
         deleteSubTaskIcon.setOnMouseClicked(event -> {
 
         });
 
+        deleteSubTaskIcon.setOnMouseClicked(event -> {
+            TaskRepository.getInstance().deleteSubTask(taskSettingsController.currentTask.getId(), getItem());
+            taskSettingsController.currentTask.getChildren().remove(getItem());
+            getListView().getItems().remove(getItem());
+        });
+
         setGraphic(subTaskBorderPane);
     }
-
 }
 
 
