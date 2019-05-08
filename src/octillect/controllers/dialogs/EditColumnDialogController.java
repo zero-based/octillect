2bccd204ff319e59045eb22b0d6522a05e15390a
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-08 05:29:11 +0200

    Add Edit Column Back-end

--- src/octillect/controllers/dialogs/EditColumnDialogController.java
+++ src/octillect/controllers/dialogs/EditColumnDialogController.java
@@ -1,15 +1,20 @@
 package octillect.controllers.dialogs;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
+import com.jfoenix.validation.RequiredFieldValidator;
 import com.jfoenix.controls.events.JFXDialogEvent;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 import octillect.controllers.ApplicationController;
+import octillect.controllers.BoardController;
 import octillect.controllers.Injectable;
 import octillect.controls.OButton;
+import octillect.database.repositories.ColumnRepository;
+import octillect.models.Column;
+import octillect.models.TaskBase;
 
 public class EditColumnDialogController implements Injectable<ApplicationController> {
 
@@ -17,24 +22,56 @@ public class EditColumnDialogController implements Injectable<ApplicationControl
     @FXML public JFXTextField editColumnTextField;
     @FXML public OButton editColumnButton;
 
+    public Column currentColumn;
+
     // Injected Controllers
     private ApplicationController applicationController;
+    private BoardController boardController;
+
+    // Empty field validation
+    private RequiredFieldValidator requiredFieldValidator;
 
     @Override
     public void inject(ApplicationController applicationController) {
         this.applicationController = applicationController;
+        boardController            = applicationController.boardController;
     }
 
     @Override
-    public void init(){
+    public void init() {
+        requiredFieldValidator = new RequiredFieldValidator("Required field.");
+        editColumnTextField.getValidators().add(requiredFieldValidator);
+        editColumnTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                editColumnTextField.validate();
+            }
+        });
     }
 
     @FXML
     public void handleEditColumnDialogClosed(JFXDialogEvent jfxDialogEvent) {
+        editColumnTextField.resetValidation();
+        editColumnTextField.setText("");
     }
 
     @FXML
     public void handleEditColumnButtonAction(ActionEvent actionEvent) {
-        editColumnDialog.close();
+        requiredFieldValidator.validate();
+        if (!requiredFieldValidator.getHasErrors()) {
+
+            ColumnRepository.getInstance().updateName(currentColumn.getId(), editColumnTextField.getText());
+
+            currentColumn.setName(editColumnTextField.getText());
+            int index = boardController.boardListView.getItems().indexOf(currentColumn);
+            boardController.boardListView.getItems().set(index, currentColumn);
+
+            for (TaskBase column : boardController.currentBoard.getChildren()) {
+                if (column.getId().equals(currentColumn.getId())) {
+                    column.setName(editColumnTextField.getText());
+                    break;
+                }
+            }
+            editColumnDialog.close();
+        }
     }
 }
