e5f4972a734a20e4d7ac89a8b8f2cee1bf5d0500
Michael Safwat <michaelsafwat.hanna@gmail.com>
2019-05-14 18:54:19 +0200

    Modify Color Palette
     * Add More Dark Color Shades.
     * Remove BlueGray Color Shades.

--- src/octillect/styles/Palette.java
+++ src/octillect/styles/Palette.java
@@ -25,21 +25,15 @@ public final class Palette {
     public static final Color WARNING;
     public static final Color DANGER;
 
+    public static final Color DARK_100;
+    public static final Color DARK_200;
     public static final Color DARK_300;
+    public static final Color DARK_400;
     public static final Color DARK_500;
+    public static final Color DARK_600;
     public static final Color DARK_700;
+    public static final Color DARK_800;
     public static final Color DARK_900;
-
-    public static final Color BLUE_GRAY_50;
-    public static final Color BLUE_GRAY_100;
-    public static final Color BLUE_GRAY_200;
-    public static final Color BLUE_GRAY_300;
-    public static final Color BLUE_GRAY_400;
-    public static final Color BLUE_GRAY_500;
-    public static final Color BLUE_GRAY_600;
-    public static final Color BLUE_GRAY_700;
-    public static final Color BLUE_GRAY_800;
-    public static final Color BLUE_GRAY_900;
 
     static {
 
@@ -74,21 +68,15 @@ public final class Palette {
         WARNING       = palette.get("-o-warning");
         DANGER        = palette.get("-o-danger");
 
+        DARK_100      = palette.get("-o-dark-100");
+        DARK_200      = palette.get("-o-dark-200");
         DARK_300      = palette.get("-o-dark-300");
+        DARK_400      = palette.get("-o-dark-400");
         DARK_500      = palette.get("-o-dark-500");
+        DARK_600      = palette.get("-o-dark-600");
         DARK_700      = palette.get("-o-dark-700");
+        DARK_800      = palette.get("-o-dark-800");
         DARK_900      = palette.get("-o-dark-900");
-
-        BLUE_GRAY_50  = palette.get("-o-blue-gray-50");
-        BLUE_GRAY_100 = palette.get("-o-blue-gray-100");
-        BLUE_GRAY_200 = palette.get("-o-blue-gray-200");
-        BLUE_GRAY_300 = palette.get("-o-blue-gray-300");
-        BLUE_GRAY_400 = palette.get("-o-blue-gray-400");
-        BLUE_GRAY_500 = palette.get("-o-blue-gray-500");
-        BLUE_GRAY_600 = palette.get("-o-blue-gray-600");
-        BLUE_GRAY_700 = palette.get("-o-blue-gray-700");
-        BLUE_GRAY_800 = palette.get("-o-blue-gray-800");
-        BLUE_GRAY_900 = palette.get("-o-blue-gray-900");
 
     }
 
@@ -109,20 +97,14 @@ public final class Palette {
     public Color getWARNING() { return WARNING; }
     public Color getDANGER()  { return DANGER; }
 
+    public Color getDARK_100() { return DARK_100; }
+    public Color getDARK_200() { return DARK_200; }
     public Color getDARK_300() { return DARK_300; }
+    public Color getDARK_400() { return DARK_400; }
     public Color getDARK_500() { return DARK_500; }
+    public Color getDARK_600() { return DARK_600; }
     public Color getDARK_700() { return DARK_700; }
+    public Color getDARK_800() { return DARK_800; }
     public Color getDARK_900() { return DARK_900; }
-
-    public Color getBLUE_GRAY_50()  { return BLUE_GRAY_50; }
-    public Color getBLUE_GRAY_100() { return BLUE_GRAY_100; }
-    public Color getBLUE_GRAY_200() { return BLUE_GRAY_200; }
-    public Color getBLUE_GRAY_300() { return BLUE_GRAY_300; }
-    public Color getBLUE_GRAY_400() { return BLUE_GRAY_400; }
-    public Color getBLUE_GRAY_500() { return BLUE_GRAY_500; }
-    public Color getBLUE_GRAY_600() { return BLUE_GRAY_600; }
-    public Color getBLUE_GRAY_700() { return BLUE_GRAY_700; }
-    public Color getBLUE_GRAY_800() { return BLUE_GRAY_800; }
-    public Color getBLUE_GRAY_900() { return BLUE_GRAY_900; }
 
 }
