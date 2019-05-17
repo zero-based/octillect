be6689a74ea80606823de45c29fdc46db583fed7
Micheline Medhat <MichelineMedhat@gmail.com>
2019-05-17 21:40:58 +0200

    Add displayErrorView method to OCTException

--- src/octillect/exceptions/OCTException.java
+++ src/octillect/exceptions/OCTException.java
@@ -1,14 +1,42 @@
 package octillect.exceptions;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
 public class OCTException extends Exception {
 
-    //Error Fields
+    // Local Fields
     private String errorCode;
     private String errorDescription;
+
+    // FXML Fields
+    @FXML private StackPane errorStackPane;
+    @FXML public Label errorLabel;
+    @FXML public Button goBackButton;
+
 
     public OCTException(String errorCode, String errorDescription) {
         this.errorCode        = errorCode;
         this.errorDescription = errorDescription;
+    }
+
+    public void dislpayError(StackPane root) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/octillect/views/ErrorView.fxml"));
+            fxmlLoader.setController(this);
+            errorStackPane = fxmlLoader.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        errorLabel.setText("[" + errorCode + "]: " + errorDescription);
+        goBackButton.setOnAction(event -> {
+            /*TODO: Add retry method here*/
+        });
+        root.getChildren().add(errorStackPane);
     }
 
     public void printError() {
